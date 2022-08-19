f51f4779d9897946ff02c5c94ff22b61d138b30b
yashp2 <yashp9145@gmail.com>
2022-08-20 04:20:15 +0530

    result_pg_updated

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -111,3 +111,4 @@ function Result() {
 }
 
 export default Result;
+{/* <div> Icons made by <a href="https://www.freepik.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div> */}
